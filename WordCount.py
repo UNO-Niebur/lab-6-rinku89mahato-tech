2b3d8a2bf51740242c199ae2533ec62eb54c7661
rinku89mahato-tech <rinku89mahato@gmail.com>
2026-02-28 18:53:32 +0000

    progressing word count

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,25 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Rinku Mahato
+#Date: 02/28/2026
+#Assignment: Word Count
 
 def main():
   textFile = open("gettysberg.txt", 'r')
-  
+  lineCount = 0 
+  wordCount = 0
+  letterCount = 0
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount + 1
+    words = line.split()
+    characters = len(line)
+    letterCount = letterCount + characters
+    print(words)
+    for w in words:
+      wordCount = wordCount + 1
+    #print(line)
+  print("Lines:", lineCount)
+  print ("words:", wordCount)
+  print("characters:", letterCount)
 
 if __name__ == '__main__':
   main()
